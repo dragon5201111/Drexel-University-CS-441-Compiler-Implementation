d71332feeff2b431109c11136f2d7aa4a5c130c2
evoexo <dragon520111back@gmail.com>
2026-01-14 20:27:45 -0500

    Started implementing statements.

--- include/Stmnt.h
+++ include/Stmnt.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Expr.h"
+
+class VariableDeclStmnt;
+
+class StmntVisitor {
+    virtual ~StmntVisitor() = default;
+
+};
+
+class Stmnt {
+public:
+    virtual ~Stmnt() = default;
+    virtual void accept(StmntVisitor& visitor) const = 0;
+};
+
+class VariableDeclStmnt: public Stmnt {
+
+};
+
